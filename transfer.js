f17265010459cef5d129e8fcb4b9348bacfd138b
Astro <astro@spaceboyz.net>
2010-10-04 22:50:08 +0200

    transfer: refactor out into base64Decoder()

--- transfer.js
+++ transfer.js
@@ -31,25 +31,23 @@ Transfer.prototype.acceptUpload = function(req, res) {
     this.upReq = req;
     this.upRes = res;
 
-    req.setEncoding('utf-8');
+    var decoder = base64Decoder();
+    req.setEncoding('utf-8');  // FIXME
+
     var buf = '';
     req.on('data', function(data) {
-	buf += data;
-	var i = Math.floor(buf.length / 4) * 4;
-	data = new Buffer(buf.slice(0, i), 'base64');
-	buf = buf.slice(i, buf.length);
-	var written = that.downRes.write(data);
-	console.log({written:written});
+	var written = that.downRes.write(decoder(data), 'binary');
 	if (!written) {
-	    req.socket.pause();
+	    req.pause();
 	    console.log('pause');
 	}
     });
-    this.downRes.socket.on('drain', function() {
+    this.downRes.on('drain', function() {
 	console.log('drain, resume');
-	req.socket.resume();
+	req.resume();
     });
     req.on('end', function() {
+	that.downRes.write(decoder('', true), 'binary');
 	that.end();
     });
 
@@ -57,6 +55,7 @@ Transfer.prototype.acceptUpload = function(req, res) {
 };
 
 Transfer.prototype.end = function() {
+console.log({transferEnd:this});
     if (this.upRes) {
 	this.upRes.end();
     } else
@@ -65,3 +64,26 @@ Transfer.prototype.end = function() {
     this.downRes.end();
 };
 
+
+function base64Decoder() {
+    var buf = '';  // string to decode
+    var _in = 0, _out = 0;
+    return function(data, flush) {
+	buf += data;
+	_in += data.length;
+
+	if (flush) {
+	    data = new Buffer(buf, 'base64');
+	    console.log('flush '+data.length);
+	    buf = '';
+	} else {
+	    var i = Math.floor(buf.length / 4) * 4;
+	    data = new Buffer(buf.slice(0, i), 'base64');
+	    buf = buf.slice(i, buf.length);
+	}
+
+	_out += data.length;
+console.log({'in':_in, out:_out});
+	return data;
+    };
+}
